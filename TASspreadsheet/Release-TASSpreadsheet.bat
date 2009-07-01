fa99cad5c318543c3447c2fbd57d2bd4a8358c0a
andres.delikat <andres.delikat@7912bdc8-64d1-11de-876f-d39545802920>
2009-07-01 03:22:27 +0000

    TASspreadsheet release.bat

---
+++ TASspreadsheet/Release-TASSpreadsheet.bat
@@ -0,0 +1,3 @@
+del ..\output\TASspreadsheet.zip
+..\ReleaseUtilities\zip -X -9 -r ..\output\TASspreadsheet.zip TASspreadsheet07.xlsx
+..\ReleaseUtilities\zip -X -9 -r ..\output\TASspreadsheet.zip TASspreadsheet.xls
