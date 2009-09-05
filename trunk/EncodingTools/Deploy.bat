d284fc101f745093758a1083718afc11054ac3c1
andres.delikat <andres.delikat@7912bdc8-64d1-11de-876f-d39545802920>
2009-09-05 02:49:08 +0000

    Encoding utilities - automate the release preparation.

--- trunk/EncodingTools/Deploy.bat
+++ trunk/EncodingTools/Deploy.bat
@@ -1,1 +1,2 @@
-zip
+..\ReleaseUtilities\zip.exe -X -9 -r EncodingUtilities.zip .\Encode\*.bat .\Encode\*.txt .\Encode\test0.sub .\MKV-segment\*.bat .\MKV-segment\*.txt
+move EncodingUtilities.zip ..\output\
