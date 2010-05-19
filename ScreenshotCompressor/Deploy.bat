ee097e64bf965afc5b62b4c6906a0a1b34f02a64
smelenchuk <smelenchuk@7912bdc8-64d1-11de-876f-d39545802920>
2010-05-19 14:26:57 +0000

    Major modifications to ScreenshotCompress to not rely on an external pngbat.bat, and to use brute force trials with random initial conditions (which has been demonstrated to yield superior file sizes).

--- ScreenshotCompressor/Deploy.bat
+++ ScreenshotCompressor/Deploy.bat
@@ -1,3 +1,3 @@
 ..\ReleaseUtilities\upx.exe .\output\ScreenshotCompress.exe
-..\ReleaseUtilities\zip.exe -X -9 -r ScreenshotCompress.zip .\output\pngbat.bat .\output\ScreenshotCompress.exe .\output\ScreenshotCompress-Documentation.txt
+..\ReleaseUtilities\zip.exe -X -9 -r ScreenshotCompress.zip .\output\ScreenshotCompress.cfg .\output\ScreenshotCompress.exe .\output\ScreenshotCompress-Documentation.txt
 move ScreenshotCompress.zip ..\output\
--- ScreenshotCompressor/ScreenshotCompress.cpp
+++ ScreenshotCompressor/ScreenshotCompress.cpp
@@ -1,9 +1,10 @@
 // SSCompress.cpp
-// written by: adelikat
-// purpose: to use pngout for optimal compression of game screenshots
+// written by: adelikat & sgrunt
+// purpose: to use pngout/deflopt for optimal compression of game screenshots
 // It is designed to be a windows application where the user simply needs to drag & drop the screenshot onto Screenshotcompress.exe
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 #include <windows.h>
 #include <Winbase.h>
@@ -13,16 +14,63 @@ using namespace std;
 
 #define sprintf sprintf_s
 
+#define DEFAULT_COUNT 100
+
 int main(int argc, char* argv[])
 {
 	bool failure = false;			//used for error checking with filestream, if this becomes true, abort program
 	string filename = "test";		//The dafult if nothing is specified by the user
 	string filenameFull;			//Will store the full path to the file that was compressed
 
+	unsigned int count = DEFAULT_COUNT; // number of trials
 	
-	
-	string testArray[16];			//Array of filename to test, will be populated based on filename, [0] will be a dummy
-	FILE* files[16];				//[0] will be dummy
+	string **testArray;			//Array of filename to test, will be populated based on filename, [0] will be a dummy
+	FILE* thisFile = NULL;
+
+	FILE*  configFile = fopen("ScreenshotCompress.cfg", "r");
+	if (configFile)
+	{
+		fscanf(configFile, "%d", &count);
+		fclose(configFile);
+		if (count > 65535)
+		{
+			cout << "Please use a sane count value (lower than 65535)!\n";
+			return 2;
+		}
+		cout << "Using " << count << " trials.\n";
+	}
+	else
+	{
+		cout << "ScreenshotCompress.cfg not found - defaulting to "
+			 << DEFAULT_COUNT << " trials.\n";
+	}
+
+	configFile = fopen("pngout.exe", "r");
+	if (configFile)
+	{
+		fclose(configFile);
+	}
+	else
+	{
+		cout << "pngout.exe not found!\n";
+		return 3;
+	}
+
+	configFile = fopen("deflopt.exe", "r");
+	if (configFile)
+	{
+		fclose(configFile);
+	}
+	else
+	{
+		cout << "deflopt.exe not found!\n";
+		return 3;
+	}
+
+
+	testArray = (string **)malloc((count+1)*sizeof(string *));
+	for (unsigned int i = 0; i <= count; i++)
+		testArray[i] = new string;
 
 	//Get base directory
 	char* getPath = (char*)malloc(2048*sizeof(char));
@@ -51,87 +99,86 @@ int main(int argc, char* argv[])
 	////////////////////////////////////////////////////////////////
 
 	//Populate the test array
-	char temp[3] = "";
-	for (int x = 1; x <= 15; x++)
+	char temp[6] = "";
+	for (unsigned int x = 1; x <= count; x++)
 	{
-		testArray[x] = baseDirectory;
-		testArray[x].append(filename);	//Add filename
-		
-		if (x < 10)
-			testArray[x].append("0");	//Make 01, 02 etc.
-
-		sprintf(temp, "%d", x);			//Append X
-		testArray[x].append(temp);
-		testArray[x].append(".png");
+		*testArray[x] = baseDirectory;
+		testArray[x]->append(filename);		//Feed pngout the filename
+		sprintf(temp, "%05d", x);			//Append X
+		testArray[x]->append(temp);
+		testArray[x]->append(".png");
 
 		//cout << testArray[x].c_str() << "\n"; //Debug
+
+		string parameters = "\"";
+		parameters.append(filenameFull.c_str());	//Send pngout the filename
+		parameters.append("\" \"");					//Enclose in quotes
+		parameters.append(*testArray[x]);
+		parameters.append("\" -r -y");
+
+		cout << "Running pngout " << parameters.c_str() << "\n";
+
+		SHELLEXECUTEINFOA ShExecInfo = {0};
+		ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+		ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+		ShExecInfo.hwnd = NULL;
+		ShExecInfo.lpVerb = NULL;
+		ShExecInfo.lpFile = "pngout.exe";		
+		ShExecInfo.lpParameters = parameters.c_str();
+		ShExecInfo.lpDirectory = baseDirectory.c_str();
+		ShExecInfo.nShow = SW_HIDE;
+		ShExecInfo.hInstApp = NULL;	
+		ShellExecuteExA(&ShExecInfo);
+		WaitForSingleObject(ShExecInfo.hProcess,INFINITE);
+
+		parameters = "\"";
+		parameters.append(*testArray[x]); //Send deflopt the filename
+		parameters.append("\"");
+
+		cout << "Running deflopt " << parameters.c_str() << "\n";
+
+		ShExecInfo.lpFile = "deflopt.exe";		
+		ShExecInfo.lpParameters = parameters.c_str();
+		ShellExecuteExA(&ShExecInfo);
+		WaitForSingleObject(ShExecInfo.hProcess,INFINITE);
 	}
-	
-	cout << "Opening pngbat.bat...\n";
 
-	string parameters = "\"";
-	parameters.append(filenameFull.c_str());	//Send pngbat.bat the filename
-	parameters.append("\"");					//Enclose in quotes
-	parameters.append(" ");
-	parameters.append(baseDirectory);			//Send the basedirectory as parameter 2 (so it places test01.png in the correct place
 	
-	cout << "With paramters: " << parameters.c_str();
-
-	SHELLEXECUTEINFOA ShExecInfo = {0};
-	ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
-	ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
-	ShExecInfo.hwnd = NULL;
-	ShExecInfo.lpVerb = NULL;
-	ShExecInfo.lpFile = "pngbat.bat";		
-	ShExecInfo.lpParameters = parameters.c_str();
-	ShExecInfo.lpDirectory = baseDirectory.c_str();
-	ShExecInfo.nShow = SW_SHOW;
-	ShExecInfo.hInstApp = NULL;	
-	ShellExecuteExA(&ShExecInfo);
-	WaitForSingleObject(ShExecInfo.hProcess,INFINITE);
 	
 	////////////////////////////////////////////////////////////////
 	//Choose the smallest file
 	////////////////////////////////////////////////////////////////
+
+	unsigned long lowestSize = 0, tempSize = 0;
+	unsigned short lowestNum = 1;
 	
-	//Open each file and do error checking
-	for (int x = 1; x <= 15; x++)
+	//Open each file, error check, and find smallest file.
+	for (unsigned int x = 1; x <= count; x++)
 	{
-		files[x] = fopen(testArray[x].c_str(), "r");
-		if (!files[x])
+		thisFile = fopen(testArray[x]->c_str(), "r");
+		if (!thisFile)
 		{
 			failure = true;
-			cout << "\n" << testArray[x].c_str() << " file was not found line56";
+			cout << "\n" << testArray[x]->c_str() << " file was not found line56";
 			break;
 		}
+
+		fseek(thisFile, 0, SEEK_END);
+		tempSize = ftell(thisFile);
+
+		if (x == 1) lowestSize = tempSize;
+		else if (tempSize < lowestSize)
+		{
+			lowestNum = x;
+			lowestSize = tempSize;
+		}
+
+		fclose(thisFile);
 	}
 	
+	// If success, rename best file
 	if (!failure)
 	{
-		//Find the lowest filesize
-		long lowestSize = 0;
-		int lowestNum = 1;
-		long tempSize;
-		for (int x = 1; x <= 15; x++)
-		{
-			fseek(files[x], 0, SEEK_END);
-			tempSize = ftell(files[x]);
-			//cout << "File number " << x << " - " << tempSize << " bytes\n"; //Debug
-			if (x == 1)
-				lowestSize = tempSize;
-			else if (tempSize < lowestSize)
-			{
-				lowestNum = x;
-				lowestSize = tempSize;
-			}			
-		}
-
-		for (int x = 1; x <= 15; x++)
-		{
-			fclose(files[x]);
-		}
-		
-
 		//cout << "\nThe lowest filsize is file number " << lowestNum << "\n" ;	//Debug
 
 		//Delete all but the smallest file
@@ -142,11 +189,11 @@ int main(int argc, char* argv[])
 		if (fail)
 			cout << "Could not remove base file\n";
 		
-		for (int x = 1; x <= 15; x++)
+		for (unsigned int x = 1; x <= count; x++)
 		{
 			if (x != lowestNum)
 			{
-				fail = remove(testArray[x].c_str());
+				fail = remove(testArray[x]->c_str());
 				//if (!fail)
 				//	cout << "Removing file " << x << "\n";
 				//else
@@ -154,10 +201,14 @@ int main(int argc, char* argv[])
 			}
 		}
 
-
 		//Rename the lowest to filename
-		rename(testArray[lowestNum].c_str(), filenameFull.c_str());
+		rename(testArray[lowestNum]->c_str(), filenameFull.c_str());
 	}
+
+	for (unsigned int i = 0; i <= count; i++)
+		delete testArray[i];
+	free(testArray);
+
 	if (failure)
 	{
 		cout << "\nCould not open 1 or more files, aborting...";
