7f758092eacaee7f0fc4c2d67eeebe413fee92ca
andres.delikat <andres.delikat@7912bdc8-64d1-11de-876f-d39545802920>
2010-05-28 22:40:44 +0000

    TAS-Movie-Editor - automate "release" process.

---
+++ trunk/TASMovieEditor/MakeRelease-TASMovieEditor.bat
@@ -0,0 +1,3 @@
+del ..\output\TAS-Movie-Editor.zip
+..\ReleaseUtilities\zip -X -9 -r ..\output\TAS-Movie-Editor .\bin\Release\tas-movie-editor.exe
+..\ReleaseUtilities\zip -X -9 -r ..\output\TAS-Movie-Editor .\bin\Release\CHANGELOG.txt
