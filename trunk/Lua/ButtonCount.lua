4b089b0def2b0db62a380b2e7e71516919ca06e7
brandman211@gmail.com <brandman211@gmail.com@7912bdc8-64d1-11de-876f-d39545802920>
2011-07-17 02:39:31 +0000

    Button Count -Made it so the script displays the button counts when you save / load a state.

--- trunk/Lua/ButtonCount.lua
+++ trunk/Lua/ButtonCount.lua
@@ -17,21 +17,26 @@ function table.copy(t)
     return t2
 end
 
+function counts()
+    --Display the counts of the holds and the presses.
+    gui.text(x, y, 'Holds: ' .. holds)
+    gui.text(x, y + 8, 'Presses: ' .. presses)
+end
+
 function load(slot)
     --As Lua starts counting from 1, and there may be a slot 0, increment.
     slot = slot + 1
-    if not states[slot] then
+    if not states[slot] or not states[slot].holds then
+        gui.text(x, y + 16, 'No data loaded from slot ' .. tostring(slot - 1))
+        counts()
         return
     end
     --Load the data if there is any available for this slot.
-    if states[slot].holds then
-        holds = states[slot].holds
-        players = table.copy(states[slot].players)
-        presses = states[slot].presses
-        gui.text(x, y + 16, 'Data loaded from slot ' .. tostring(slot - 1))
-        return
-    end
-    gui.text(x, y + 16, 'No data loaded from slot ' .. tostring(slot - 1))
+    holds = states[slot].holds
+    players = table.copy(states[slot].players)
+    presses = states[slot].presses
+    gui.text(x, y + 16, 'Data loaded from slot ' .. tostring(slot - 1))
+    counts()
 end
 
 function parse()
@@ -107,6 +112,7 @@ function save(slot)
     states[slot].players = table.copy(players)
     states[slot].presses = presses
     gui.text(x, y + 16, 'Data saved to slot ' .. tostring(slot - 1))
+    counts()
 end
 
 if parse() then
@@ -124,6 +130,7 @@ while true do
         players = {}
         presses = 0
     end
+    joypad.set(1, {B=true, right=true})
     --Check players one and two.
     for player = 1, 2 do
         local buttons = joypad.getdown(player)
@@ -141,7 +148,6 @@ while true do
         --Mark these buttons as pressed.
         players[player] = buttons
     end
-    gui.text(x, y, 'Holds: ' .. holds)
-    gui.text(x, y + 8, 'Presses: ' .. presses)
+    counts()
     emu.frameadvance()
 end
