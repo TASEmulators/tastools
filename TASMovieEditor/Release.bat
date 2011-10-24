5853a4d64bf67dc82cbeb7dc1f6392049a97cda5
aquanull <aquanull@7912bdc8-64d1-11de-876f-d39545802920>
2011-10-24 04:34:07 +0000

    - ETC::CHANGELOG.TXT

--- TASMovieEditor/Release.bat
+++ TASMovieEditor/Release.bat
@@ -1,3 +1,8 @@
 del ..\output\TAS-Movie-Editor.zip
-..\ReleaseUtilities\zip -X -9 -r ..\output\TAS-Movie-Editor .\bin\Release\tas-movie-editor.exe
-..\ReleaseUtilities\zip -X -9 -r ..\output\TAS-Movie-Editor .\bin\Release\CHANGELOG.txt
+
+copy /y .\CHANGELOG.txt .\Bin\Release\CHANGELOG.txt
+
+cd .\bin
+..\..\ReleaseUtilities\zip -X -9 -r ..\..\output\TAS-Movie-Editor .\Release\tas-movie-editor.exe
+..\..\ReleaseUtilities\zip -X -9 -r ..\..\output\TAS-Movie-Editor .\Release\CHANGELOG.txt
+cd..
