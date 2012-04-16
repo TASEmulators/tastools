b4c75b7344ec43276f02a7b84c14f703024fc97b
andres.delikat <andres.delikat@7912bdc8-64d1-11de-876f-d39545802920>
2012-04-16 18:41:24 +0000

    ScreenshotCompress - allow queuing of files (multiple files in commandline args or drag & drop

--- trunk/ScreenshotCompressor/ScreenshotCompress.cpp
+++ trunk/ScreenshotCompressor/ScreenshotCompress.cpp
@@ -85,148 +85,150 @@ int main(int argc, char* argv[])
 		return 3;
 	}
 
-	
-	
-	testArray = (string **)malloc((count+1)*sizeof(string *));
-	for (unsigned int i = 0; i <= count; i++)
-		testArray[i] = new string;
-
-	cout << "Working directory is:\n" << baseDirectory.c_str() << "\n\n"; //Debug
-
-	if (argc > 1)
-		filenameFull = argv[1];		//Get a filename if user chose one
-	else
+	if (argc <= 1)
 	{
 		filenameFull = baseDirectory;
 		filenameFull.append(filename);	//If filename isn't specified use test.png in the same dictory as the executable
 		filenameFull.append(".png");
 	}
 
-
-	cout << "Filename is:\n" << filenameFull.c_str() << "\n\n"; //debug
-	////////////////////////////////////////////////////////////////
-	// Compress screenshot into 16 files
-	////////////////////////////////////////////////////////////////
-
-	//Populate the test array
-	char temp[6] = "";
-	for (unsigned int x = 1; x <= count; x++)
+	//////////////////////////////////////////////////////////////////////
+	for (int z = 1; z < argc; z++)
 	{
-		*testArray[x] = baseDirectory;
-		testArray[x]->append(filename);		//Feed pngout the filename
-		sprintf(temp, "%05d", x);			//Append X
-		testArray[x]->append(temp);
-		testArray[x]->append(".png");
+		testArray = (string **)malloc((count+1)*sizeof(string *));
+		for (unsigned int i = 0; i <= count; i++)
+			testArray[i] = new string;
 
-		//cout << testArray[x].c_str() << "\n"; //Debug
+		cout << "Working directory is:\n" << baseDirectory.c_str() << "\n\n"; //Debug
 
-		string parameters = "\"";
-		parameters.append(filenameFull.c_str());	//Send pngout the filename
-		parameters.append("\" \"");					//Enclose in quotes
-		parameters.append(*testArray[x]);
-		parameters.append("\" -r -y");
+		filenameFull = argv[z];
 
-		cout << "Running pngout " << parameters.c_str() << "\n";
+		cout << "Filename is:\n" << filenameFull.c_str() << "\n\n"; //debug
+		////////////////////////////////////////////////////////////////
+		// Compress screenshot into 16 files
+		////////////////////////////////////////////////////////////////
 
-		SHELLEXECUTEINFOA ShExecInfo = {0};
-		ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
-		ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
-		ShExecInfo.hwnd = NULL;
-		ShExecInfo.lpVerb = NULL;
-		ShExecInfo.lpFile = "pngout.exe";		
-		ShExecInfo.lpParameters = parameters.c_str();
-		ShExecInfo.lpDirectory = baseDirectory.c_str();
-		ShExecInfo.nShow = SW_HIDE;
-		ShExecInfo.hInstApp = NULL;	
-		ShellExecuteExA(&ShExecInfo);
-		WaitForSingleObject(ShExecInfo.hProcess,INFINITE);
-
-		parameters = "\"";
-		parameters.append(*testArray[x]); //Send deflopt the filename
-		parameters.append("\"");
-
-		cout << "Running deflopt " << parameters.c_str() << "\n";
-
-		ShExecInfo.lpFile = "deflopt.exe";		
-		ShExecInfo.lpParameters = parameters.c_str();
-		ShellExecuteExA(&ShExecInfo);
-		WaitForSingleObject(ShExecInfo.hProcess,INFINITE);
-	}
-
-	
-	
-	////////////////////////////////////////////////////////////////
-	//Choose the smallest file
-	////////////////////////////////////////////////////////////////
-
-	unsigned long lowestSize = 0, tempSize = 0;
-	unsigned short lowestNum = 1;
-	
-	//Open each file, error check, and find smallest file.
-	for (unsigned int x = 1; x <= count; x++)
-	{
-		thisFile = fopen(testArray[x]->c_str(), "r");
-		if (!thisFile)
-		{
-			failure = true;
-			cout << "\n" << testArray[x]->c_str() << " file was not found line56";
-			break;
-		}
-
-		fseek(thisFile, 0, SEEK_END);
-		tempSize = ftell(thisFile);
-
-		if (x == 1) lowestSize = tempSize;
-		else if (tempSize < lowestSize)
-		{
-			lowestNum = x;
-			lowestSize = tempSize;
-		}
-
-		fclose(thisFile);
-	}
-	
-	// If success, rename best file
-	if (!failure)
-	{
-		//cout << "\nThe lowest filsize is file number " << lowestNum << "\n" ;	//Debug
-
-		//Delete all but the smallest file
-		int fail = 0;
-		
-		fail = remove(filenameFull.c_str());
-
-		if (fail)
-			cout << "Could not remove base file\n";
-		
+		//Populate the test array
+		char temp[6] = "";
 		for (unsigned int x = 1; x <= count; x++)
 		{
-			if (x != lowestNum)
-			{
-				fail = remove(testArray[x]->c_str());
-				//if (!fail)
-				//	cout << "Removing file " << x << "\n";
-				//else
-				//	cout << "Could not remove file " << x << "\n";
-			}
+			*testArray[x] = baseDirectory;
+			testArray[x]->append(filename);		//Feed pngout the filename
+			sprintf(temp, "%05d", x);			//Append X
+			testArray[x]->append(temp);
+			testArray[x]->append(".png");
+
+			//cout << testArray[x].c_str() << "\n"; //Debug
+
+			string parameters = "\"";
+			parameters.append(filenameFull.c_str());	//Send pngout the filename
+			parameters.append("\" \"");					//Enclose in quotes
+			parameters.append(*testArray[x]);
+			parameters.append("\" -r -y");
+
+			cout << "Running pngout " << parameters.c_str() << "\n";
+
+			SHELLEXECUTEINFOA ShExecInfo = {0};
+			ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+			ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+			ShExecInfo.hwnd = NULL;
+			ShExecInfo.lpVerb = NULL;
+			ShExecInfo.lpFile = "pngout.exe";		
+			ShExecInfo.lpParameters = parameters.c_str();
+			ShExecInfo.lpDirectory = baseDirectory.c_str();
+			ShExecInfo.nShow = SW_HIDE;
+			ShExecInfo.hInstApp = NULL;	
+			ShellExecuteExA(&ShExecInfo);
+			WaitForSingleObject(ShExecInfo.hProcess,INFINITE);
+
+			parameters = "\"";
+			parameters.append(*testArray[x]); //Send deflopt the filename
+			parameters.append("\"");
+
+			cout << "Running deflopt " << parameters.c_str() << "\n";
+
+			ShExecInfo.lpFile = "deflopt.exe";		
+			ShExecInfo.lpParameters = parameters.c_str();
+			ShellExecuteExA(&ShExecInfo);
+			WaitForSingleObject(ShExecInfo.hProcess,INFINITE);
 		}
 
-		//Rename the lowest to filename
-		rename(testArray[lowestNum]->c_str(), filenameFull.c_str());
-	}
-
-	for (unsigned int i = 0; i <= count; i++)
-		delete testArray[i];
-	free(testArray);
-
 	
-	if (failure)
-	{
-		cout << "\nCould not open 1 or more files, aborting...";
-		cout << "\nPress Enter key to exit";
-		cin.ignore(cin.rdbuf()->in_avail()+1);
-		return 1;
+	
+		////////////////////////////////////////////////////////////////
+		//Choose the smallest file
+		////////////////////////////////////////////////////////////////
+
+		unsigned long lowestSize = 0, tempSize = 0;
+		unsigned short lowestNum = 1;
+	
+		//Open each file, error check, and find smallest file.
+		for (unsigned int x = 1; x <= count; x++)
+		{
+			thisFile = fopen(testArray[x]->c_str(), "r");
+			if (!thisFile)
+			{
+				failure = true;
+				cout << "\n" << testArray[x]->c_str() << " file was not found line56";
+				break;
+			}
+
+			fseek(thisFile, 0, SEEK_END);
+			tempSize = ftell(thisFile);
+
+			if (x == 1) lowestSize = tempSize;
+			else if (tempSize < lowestSize)
+			{
+				lowestNum = x;
+				lowestSize = tempSize;
+			}
+
+			fclose(thisFile);
+		}
+	
+		// If success, rename best file
+		if (!failure)
+		{
+			//cout << "\nThe lowest filsize is file number " << lowestNum << "\n" ;	//Debug
+
+			//Delete all but the smallest file
+			int fail = 0;
+		
+			fail = remove(filenameFull.c_str());
+
+			if (fail)
+				cout << "Could not remove base file\n";
+		
+			for (unsigned int x = 1; x <= count; x++)
+			{
+				if (x != lowestNum)
+				{
+					fail = remove(testArray[x]->c_str());
+					//if (!fail)
+					//	cout << "Removing file " << x << "\n";
+					//else
+					//	cout << "Could not remove file " << x << "\n";
+				}
+			}
+
+			//Rename the lowest to filename
+			rename(testArray[lowestNum]->c_str(), filenameFull.c_str());
+		}
+
+		for (unsigned int i = 0; i <= count; i++)
+			delete testArray[i];
+		free(testArray);
+	
+		if (failure)
+		{
+			cout << "\nCould not open 1 or more files, aborting...";
+			cout << "\nPress Enter key to exit";
+			cin.ignore(cin.rdbuf()->in_avail()+1);
+			return 1;
+		}
 	}
+	//////////////////////////////////////////////////////////////////////
+
 	//Keep window open until user presses enter
 	//cout << "\nPress Enter key to exit";
 	//cin.ignore(cin.rdbuf()->in_avail()+1);
